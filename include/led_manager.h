af8b29ad75aa457141e7e0f97d450432bd6bd495
henrylewis2004 <henry@lewis-lewis.co.uk>
2026-02-20 19:03:48 +0000

    led blink

--- include/led_manager.h
+++ include/led_manager.h
@@ -1,16 +1,7 @@
-#ifndef LED_DELAY_MS
-#define LED_DELAY_MS 250
-#endif
-
-#ifdef CYW43_WL_GPIO_LED_PIN
-#include "pico/cyw43_arch.h" 
-#endif
-
 #ifndef LED_MANAGER_H
 #define LED_MANAGER_H
 
-void ldm_led_init(void);
-void ldm_led_set(bool led_on);
-void ldm_led_blink(void);
+void blink(void);
+int led_init(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,47 +1,12 @@
 //standard C libraries
 //#include <stdio.h>
 //pico libraries
-#include "pico/stdlib.h"
+//#include "pico/stdlib.h"
 //project libraries
-//#include "led_manager.h"
+#include "led_manager.h"
 
-//
-#ifdef CYW43_WL_GPIO_LED_PIN
-#include "pico/cyw43_arch.h" 
-#endif
 
-#ifndef LED_DELAY_MS
-#define LED_DELAY_MS 250
-#endif
-
-//
-int Pico_led_init(void){
-#if defined(PICO_DEFAULT_LED_PIN)
-	gpio_init(PICO_DEFAULT_LED_PIN);
-	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-	return PICO_OK;
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-	return cyw43_arch_init();
-#endif
-}
-
-void Pico_set_led(bool led_on){
-#if defined(PICO_DEFAULT_LED_PIN)
-	gpio_put(PICO_DEFAULT_LED_PIN,led_on);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN,led_on);
-#endif
-}
-
-int main(){
-	int rc = Pico_led_init();
-	hard_assert(rc = PICO_OK);
-
-	while(true){
-		Pico_set_led(true);
-		sleep_ms(250);
-		Pico_set_led(false);
-		sleep_ms(250);
-	}
-
+int main(void){
+	blink();
+	for(;;);
 }
