e987a45c228f6c83fd3ebd2c1d600cac21a00a71
HENRY LEWIS <2520541@chester.ac.uk>
2026-03-03 23:59:21 +0000

    audio

--- src/audio_player.c
+++ src/audio_player.c
@@ -2,12 +2,38 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+//pico headers
+#include "pico/stdlib.h"
+
 //project libraries
 #include "audio_player.h"
 #include "audio_dac_bridge.h"
 
+#ifndef SAMPLERATE
+#define SAMPLE_RATE 44100
+#endif
+
+#ifndef SINE_FREQ 
+#define SINE_FREQ 440
+#endif
+
+#ifndef BUF_SAMPLES 
+#define BUF_SAMPLES 1024
+#endif
 
 // internal \\
+
+int16_t sine_buf[BUF_SAMPLES];
+
+static void generate_sine(void){
+	for (int i = 0; i < BUF_SAMPLES; i++){
+		sine_buf[i] = (int16_t)(32767.0f *
+			sinf(2.0f * M_PI * SINE_FREQ * i / (float)SAMPLE_RATE)
+		)
+
+	}
+
+}
 
 //public
 
